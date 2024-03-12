4faf512de1366cebfd6453f3a75113a2ce344e2e
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2024-03-12 11:18:38 +0000

    feat(MovingAvg): Converted moving avg to a template class
    This was changes to a template class since the way it works is the same and i will be needing multiple moving avereges, and some of them wont be storing doubles, but integers
    
    Signed-off-by: andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>

--- part1/include/moving_avg.hpp
+++ part1/include/moving_avg.hpp
@@ -1,24 +1,28 @@
 #ifndef __MOVING_AVG_HPP__
 #define __MOVING_AVG_HPP__
 
-#include <vector>
+#include <array>
 #include <algorithm>
 #include <numeric>
 
+template <typename T, size_t size>
 class MovingAvg
 {
 public:
-    MovingAvg(unsigned int size);
-    ~MovingAvg();
+    inline void operator<<(T value)
+    {
+        values[index++] = value;
+        index %= size;
+    }
 
-    void operator<<(double value);
-    float get_avg();
+    inline T get_avg()
+    {
+        return std::accumulate(values.begin(), values.end(), 0.0) / (static_cast<T>(size));
+    }
 
-    std::vector<double> values;
-    unsigned int size;
+    std::array<T, size> values = {0};
 
 private:
     unsigned int index = 0;
 };
-
 #endif // __MOVING_AVG_HPP__
